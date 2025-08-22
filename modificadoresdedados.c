f346320659749a36f83120f637f0cf23058bd630
Tiago Dourado da Silva <tdourado412@gmail.com>
2025-08-22 16:59:13 +0000

    unsigned

---
+++ modificadoresdedados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+     int numeroSinal = 3000000000;
+     unsigned int numeroSemSinal = 3000000000;
+
+     printf("Número com sinal: %d\n", numeroSinal);
+     printf("Número sem sinal: %u\n", numeroSemSinal);
+
+     return 0;
+     
+}
