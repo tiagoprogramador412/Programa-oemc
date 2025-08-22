e3690254dcea877ce7f9b5fc3d91d656eea65bb5
Tiago Dourado da Silva <tdourado412@gmail.com>
2025-08-22 16:36:04 +0000

    Aula concluida

---
+++ .vscode/int+int=float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 3;
+    float quociente = (float) a / b;
+
+    printf("Quociente: %.2f\n", quociente);
+
+    return 0;
+
+}
---
+++ operacaomatematica3.c/pontoflutuante.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    float x = 5.5;
+    float y = 2.2;
+    float soma = x + y;
+    float diferenca = x - y;
+    float produto = x * y;
+    float quociente = x / y;
+
+    printf("Soma: %.2f\n", soma);
+    printf("Diferença: %.2f\n", diferenca);
+    printf("Produto: %.2f\n", produto);
+    printf("Quociente: %.2f\n", quociente);  
+
+    return 0;
+
+}
